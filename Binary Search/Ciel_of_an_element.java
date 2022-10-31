db1827dc929ce029350912c55b6463de1b6726a8
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2022-10-31 10:14:31 -0700

    Add files via upload

--- Binary Search/Ciel_of_an_element.java
+++ Binary Search/Ciel_of_an_element.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
-public class Floor_Of_an_element{
+public class Ciel_of_an_element {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a[] = new int[n];
-        for (int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i < n ; i++){
             a[i] = sc.nextInt();
+
         }
         int find = sc.nextInt();
-        System.out.println(Floor(a , find));
+        System.out.println(Ciel(a , find));
         sc.close();
-    }
-    public static int Floor(int a[] , int find){
-        int ans = -1;
-        int si = 0;
-        int ei = a.length-1;
+    }  
+    public static int Ciel(int a[] , int find){
+        int ans  = -1;
+        int si = 0 , ei = a.length-1;
         while(si <= ei){
-            int mid = (ei-si)/2 + si;
+            int mid = (ei-si)/2 +si;
             if(a[mid] == find){
                 return mid;
             }
             if(a[mid] < find){
-                ans = mid;
                 si = mid+1;
             }
-            else if(a[mid] > find){
+            else{
+                ans = mid;
                 ei = mid-1;
             }
         }
